a9a5026fbb605babf42c2cfe081360466b660a66
lgaetz <lgaetz@gmail.com>
2013-06-15 13:41:10 -0300

    ver 0.0.3 for release

--- functions.inc.php
+++ functions.inc.php
@@ -68,15 +68,16 @@ function simplecf_scf_toggle($c) {
 	$id = "app-scf-toggle"; // The context to be included
 
 	$ext->addInclude('from-internal-additional', $id); // Add the include from from-internal
-
 	$ext->add($id, $c, '', new ext_answer(''));
 	$ext->add($id, $c, '', new ext_wait('1'));
 	$ext->add($id, $c, '', new ext_macro('user-callerid'));
 	$ext->add($id, $c, '', new ext_setvar('fromext', '${AMPUSER}'));
-
-	$ext->add($id, $c, '', new ext_gotoif('$["${DB(CF/${fromext})}" = ""]', 'activate', 'deactivate'));
-
-
+	$ext->add($id, $c, '', new ext_gotoif('$["${DB(SCF/${fromext})}"!=""]', 'continue'));
+	$ext->add($id, $c, '', new ext_playback('call-forward&number&for&extension'));
+	$ext->add($id, $c, '', new ext_saydigits('${fromext}'));
+	$ext->add($id, $c, '', new ext_playback('not-yet-assigned&goodbye'));
+	$ext->add($id, $c, '', new ext_macro('hangupcall'));
+	$ext->add($id, $c, 'continue', new ext_gotoif('$["${DB(CF/${fromext})}" = ""]', 'activate', 'deactivate'));
 	$ext->add($id, $c, 'activate', new ext_setvar('toext', '${DB(SCF/${fromext})}'));
 	$ext->add($id, $c, '', new ext_gotoif('$["${toext}"=""]', 'activate'));
 	$ext->add($id, $c, '', new ext_wait('1')); // $cmd,n,Wait(1)
@@ -88,17 +89,17 @@ function simplecf_scf_toggle($c) {
 	if ($amp_conf['FCBEEPONLY']) {
 		$ext->add($id, $c, 'hook_on', new ext_playback('beep')); // $cmd,n,Playback(...)
 	} else {
-	  $ext->add($id, $c, 'hook_on', new ext_playback('call-fwd-unconditional&for&extension'));
+	  $ext->add($id, $c, 'hook_on', new ext_playback('call-fwd-unconditional&activated&for&extension'));
 	  $ext->add($id, $c, '', new ext_saydigits('${fromext}'));
 	  $ext->add($id, $c, '', new ext_playback('is-set-to'));
 	  $ext->add($id, $c, '', new ext_saydigits('${toext}'));
+	  $ext->add($id, $c, '', new ext_playback('goodbye'));
 	}
 	$ext->add($id, $c, '', new ext_macro('hangupcall'));
 	$ext->add($id, $c, 'setdirect', new ext_answer(''));
 	$ext->add($id, $c, '', new ext_wait('1'));
 	$ext->add($id, $c, '', new ext_macro('user-callerid'));
 	$ext->add($id, $c, '', new ext_goto('toext'));
-
 	$ext->add($id, $c, 'deactivate', new ext_dbdel('CF/${fromext}')); 
 	if ($amp_conf['USEDEVSTATE']) {
 		$ext->add($id, $c, '', new ext_setvar('STATE', 'NOT_INUSE'));
@@ -134,26 +135,187 @@ function simplecf_scf_define($c) {
 	$id = "app-scf-define"; // The context to be included
 
 	$ext->addInclude('from-internal-additional', $id); // Add the include from from-internal
-
 	$ext->add($id, $c, '', new ext_answer('')); // $cmd,1,Answer
 	$ext->add($id, $c, '', new ext_wait('1')); // $cmd,n,Wait(1)
 	$ext->add($id, $c, '', new ext_macro('user-callerid')); // $cmd,n,Macro(user-callerid)
-
-
 	$ext->add($id, $c, '', new ext_setvar('fromext', '${AMPUSER}'));	
 	$ext->add($id, $c, '', new ext_wait('1')); // $cmd,n,Wait(1)
 	$ext->add($id, $c, 'startread', new ext_read('toext', 'ent-target-attendant&then-press-pound'));
-	
 	$ext->add($id, $c, '', new ext_gotoif('$["foo${toext}"="foo"]', 'startread'));
 	$ext->add($id, $c, '', new ext_wait('1')); // $cmd,n,Wait(1)
 	$ext->add($id, $c, '', new ext_setvar('DB(SCF/${fromext})', '${toext}')); 
 	if ($amp_conf['FCBEEPONLY']) {
 		$ext->add($id, $c, 'hook_1', new ext_playback('beep')); // $cmd,n,Playback(...)
 	} else {
-	  $ext->add($id, $c, 'hook_1', new ext_playback('call-fwd-unconditional&for&extension'));
-	  $ext->add($id, $c, '', new ext_saydigits('${fromext}'));
-	  $ext->add($id, $c, '', new ext_playback('is-set-to'));
-	  $ext->add($id, $c, '', new ext_saydigits('${toext}'));
+		$ext->add($id, $c, 'hook_1', new ext_playback('call-fwd-unconditional&for&extension'));
+		$ext->add($id, $c, '', new ext_saydigits('${fromext}'));
+		$ext->add($id, $c, '', new ext_playback('is-set-to'));
+		$ext->add($id, $c, '', new ext_saydigits('${toext}'));
+		$ext->add($id, $c, '', new ext_playback('goodbye'));
 	}
-	$ext->add($id, $c, '', new ext_macro('hangupcall')); // $cmd,n,Macro(user-callerid)
+
+$ext->add($id, $c, '', new ext_gotoif('$["${DB(CF/${fromext})}"=""]', 'done'));
+$ext->add($id, $c, '', new ext_setvar('DB(CF/${fromext})', '${DB(SCF/${fromext})}'));
+
+	$ext->add($id, $c, 'done', new ext_macro('hangupcall')); // $cmd,n,Macro(user-callerid)
+}
+
+function simplecf_vercheck() {
+	$newver = false;
+	if ( function_exists(simplecf_xml2array)){
+		$module_local = simplecf_xml2array("modules/simplecf/module.xml");
+		$module_remote = simplecf_xml2array("https://raw.github.com/POSSA/freepbx-Simple_Call_Forward/master/module.xml");
+		if ( $module_remote[module][version] > $module_local[module][version])
+			{
+			$newver = true;
+			}
+		return ($newver);
+		}
+	}
+
+//Parse XML file into an array
+function simplecf_xml2array($url, $get_attributes = 1, $priority = 'tag')  {
+	$contents = "";
+	if (!function_exists('xml_parser_create'))
+	{
+		return array ();
+	}
+	$parser = xml_parser_create('');
+	if(!($fp = @ fopen($url, 'rb')))
+	{
+		return array ();
+	}
+	while(!feof($fp))
+	{
+		$contents .= fread($fp, 8192);
+	}
+	fclose($fp);
+	xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8");
+	xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+	xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+	xml_parse_into_struct($parser, trim($contents), $xml_values);
+	xml_parser_free($parser);
+	if(!$xml_values)
+	{
+		return; //Hmm...
+	}
+	$xml_array = array ();
+	$parents = array ();
+	$opened_tags = array ();
+	$arr = array ();
+	$current = & $xml_array;
+	$repeated_tag_index = array ();
+	foreach ($xml_values as $data)
+	{
+		unset ($attributes, $value);
+		extract($data);
+		$result = array ();
+		$attributes_data = array ();
+		if (isset ($value))
+		{
+			if($priority == 'tag')
+			{
+				$result = $value;
+			}
+			else
+			{
+				$result['value'] = $value;
+			}
+		}
+		if(isset($attributes) and $get_attributes)
+		{
+			foreach($attributes as $attr => $val)
+			{
+				if($priority == 'tag')
+				{
+					$attributes_data[$attr] = $val;
+				}
+				else
+				{
+					$result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+				}
+			}
+		}
+		if ($type == "open")
+		{
+			$parent[$level -1] = & $current;
+			if(!is_array($current) or (!in_array($tag, array_keys($current))))
+			{
+				$current[$tag] = $result;
+				if($attributes_data)
+				{
+					$current[$tag . '_attr'] = $attributes_data;
+				}
+				$repeated_tag_index[$tag . '_' . $level] = 1;
+				$current = & $current[$tag];
+			}
+			else
+			{
+				if (isset ($current[$tag][0]))
+				{
+					$current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
+					$repeated_tag_index[$tag . '_' . $level]++;
+				}
+				else
+				{
+					$current[$tag] = array($current[$tag],$result);
+					$repeated_tag_index[$tag . '_' . $level] = 2;
+					if(isset($current[$tag . '_attr']))
+					{
+						$current[$tag]['0_attr'] = $current[$tag . '_attr'];
+						unset ($current[$tag . '_attr']);
+					}
+				}
+				$last_item_index = $repeated_tag_index[$tag . '_' . $level] - 1;
+				$current = & $current[$tag][$last_item_index];
+			}
+		}
+		else if($type == "complete")
+		{
+			if(!isset ($current[$tag]))
+			{
+				$current[$tag] = $result;
+				$repeated_tag_index[$tag . '_' . $level] = 1;
+				if($priority == 'tag' and $attributes_data)
+				{
+					$current[$tag . '_attr'] = $attributes_data;
+				}
+			}
+			else
+			{
+				if (isset ($current[$tag][0]) and is_array($current[$tag]))
+				{
+					$current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
+					if ($priority == 'tag' and $get_attributes and $attributes_data)
+					{
+						$current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
+					}
+					$repeated_tag_index[$tag . '_' . $level]++;
+				}
+				else
+				{
+					$current[$tag] = array($current[$tag],$result);
+					$repeated_tag_index[$tag . '_' . $level] = 1;
+					if ($priority == 'tag' and $get_attributes)
+					{
+						if (isset ($current[$tag . '_attr']))
+						{
+							$current[$tag]['0_attr'] = $current[$tag . '_attr'];
+							unset ($current[$tag . '_attr']);
+						}
+						if ($attributes_data)
+						{
+							$current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
+						}
+					}
+					$repeated_tag_index[$tag . '_' . $level]++; //0 and 1 index is already taken
+				}
+			}
+		}
+		else if($type == 'close')
+		{
+			$current = & $parent[$level -1];
+		}
+	}
+	return ($xml_array);
 }
---
+++ page.simplecf.php
@@ -0,0 +1,25 @@
+<?php
+//Check if user is "logged in"
+if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
+
+// check to see if user has automatic updates enabled in FreePBX settings
+$cm =& cronmanager::create($db);
+$online_updates = $cm->updates_enabled() ? true : false;
+
+// check dev site to see if new version of module is available
+if ($online_updates && $foo = simplecf_vercheck()) {
+	print "<br>A <b>new version of this module is available</b> from the <a target='_blank' href='http://pbxossa.org'>PBX Open Source Software Alliance</a><br>";
+}
+
+// get version number for display in page footer
+$module_local = simplecf_xml2array("modules/simplecf/module.xml");
+
+
+?>
+<h2>Simple Call Forwarding</h2>
+
+This module creates two new feature codes that simplify call forwarding for the casual user. One feature code sets the Call Forward Unconditional destination once, and the other feature code toggles CF to that destination on and off. A BLF hint is created for SIP extensions.</p>
+Use the Feature Codes page to see or change the Simple CF feature codes.</p></p> 
+
+<p align="center" style="font-size:11px;">Simple Call Forwarding Module version <?php echo  $module_local[module][version];  ?><br>
+This module is maintained by the developer community at the <a target="_blank" href="http://pbxossa.org"> PBX Open Source Software Alliance</a>
